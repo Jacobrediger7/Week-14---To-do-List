1981ed6049b4cb3289aea433dd6909f16414e9dc
Jacobrediger7 <jacobrediger26@gmail.com>
2025-02-07 15:46:30 -0700

    Create Tasks.js

---
+++ Tasks.js
@@ -0,0 +1,10 @@
+// 1. Create an array of task objects that will serve as test data
+const tasks = [
+    { id: 1, name: "Buy groceries", completed: false },  // Task 1
+    { id: 2, name: "Walk the dog", completed: true },    // Task 2
+    { id: 3, name: "Finish homework", completed: false }  // Task 3
+  ];
+  
+  // 2. Export the tasks array so it can be imported in other files
+  export default tasks;
+  
